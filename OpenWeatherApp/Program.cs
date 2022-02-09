85828816b84b73db46e01beaf56458005bffad08
Ebo Onwunali <ebo@njgeneral.com>
2022-02-09 16:52:26 -0500

    Finished code for API call and user intereaction

---
+++ OpenWeatherApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace OpenWeatherApp
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("OpenWeatherApiKey").ToString();
+
+            Console.WriteLine($"\nHello there! I was created to help you get the temperature of your current location. But, I need your help...\n");
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Enter the zip code of your current location\n");
+            var zipCode = Console.ReadLine();
+
+            string apiCall = $"http://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+
+            Thread.Sleep(1500);
+
+            Console.WriteLine($"\nIt is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location\n");
+        }
+
+        
+    }
+}
---
+++ OpenWeatherApp/WeatherMap.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherApp
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temperature = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temperature;
+        }
+       
+    }
+}
